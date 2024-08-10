bdde7f537d53c61ac49731fcef180477cd61d3e7
mk26710 <type-iv@mk26710.dev>
2024-08-10 19:21:08 +0400

    refactor: code splitting for hide command

--- cmd/hide.go
+++ cmd/hide.go
@@ -19,10 +19,10 @@ var hideCmd = &cobra.Command{
 	Use:   "hide [path to diretory]",
 	Short: "Hides files in specified direcatory (not recursive) and creates a map.json in it",
 	Args:  cobra.MinimumNArgs(1),
-	Run:   hideRunner,
+	RunE:  hideRunner,
 }
 
-func CreateHideEntry(targetAbs string, file fs.DirEntry) (models.MasqueEntry, error) {
+func CreateMasqueEntry(targetAbs string, file fs.DirEntry) (models.MasqueEntry, error) {
 	if file.IsDir() {
 		return models.MasqueEntry{}, fmt.Errorf("%s is not a directory", file)
 	}
@@ -42,35 +42,14 @@ func CreateHideEntry(targetAbs string, file fs.DirEntry) (models.MasqueEntry, er
 	return result, nil
 }
 
-func hideRunner(cmd *cobra.Command, args []string) {
-	if len(args) == 0 {
-		cmd.PrintErrln("You must provide a path to target directory")
-		return
-	}
-
-	targetDir := args[0]
-
-	if exists := helpers.DirExists(targetDir); !exists {
-		cmd.PrintErrln("You have provided an invalid directory path!")
-		cmd.PrintErrf("Provided path: %s\n", targetDir)
-		return
-	}
-
-	targetAbs, err := filepath.Abs(targetDir)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
+func CreateAllMasqueEntries(targetAbs string) ([]models.MasqueEntry, error) {
 	files, err := os.ReadDir(targetAbs)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return []models.MasqueEntry{}, err
 	}
 
 	if masqued := helpers.IsMasqued(targetAbs); masqued {
-		cmd.Printf("%s is already masqued!\n", targetAbs)
-		return
+		return []models.MasqueEntry{}, fmt.Errorf("%s is already masqued", targetAbs)
 	}
 
 	var entries []models.MasqueEntry
@@ -80,30 +59,66 @@ func hideRunner(cmd *cobra.Command, args []string) {
 			continue
 		}
 
-		if entry, err := CreateHideEntry(targetAbs, file); err == nil {
+		if entry, err := CreateMasqueEntry(targetAbs, file); err == nil {
 			entries = append(entries, entry)
 		}
 	}
 
-	j, err := json.MarshalIndent(entries, "", "  ")
-	if err != nil {
-		cmd.PrintErrln(err)
-		return
-	}
+	return entries, nil
+}
 
-	mapPath := path.Join(targetAbs, "map.masque.json")
-	os.WriteFile(mapPath, j, 0644)
+func HideMasqueEntris(targetAbs string, entries []models.MasqueEntry) []error {
+	var errors []error
 
 	for _, entry := range entries {
 		oldpath := path.Join(targetAbs, entry.OldName)
 		newpath := path.Join(targetAbs, entry.NewName)
 
 		if err := os.Rename(oldpath, newpath); err != nil {
-			cmd.Println(err)
+			errors = append(errors, err)
 		}
 	}
 
+	return errors
+}
+
+func hideRunner(cmd *cobra.Command, args []string) error {
+	if len(args) == 0 {
+		return fmt.Errorf("you must provide a path to target directory")
+	}
+
+	targetDir := args[0]
+
+	if exists := helpers.DirExists(targetDir); !exists {
+		return fmt.Errorf("tou have provided an invalid directory path")
+	}
+
+	targetAbs, err := filepath.Abs(targetDir)
+	if err != nil {
+		return err
+	}
+
+	entries, err := CreateAllMasqueEntries(targetAbs)
+	if err != nil {
+		return err
+	}
+
+	j, err := json.MarshalIndent(entries, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	mapPath := path.Join(targetAbs, "map.masque.json")
+	os.WriteFile(mapPath, j, 0644)
+
+	errors := HideMasqueEntris(targetAbs, entries)
+	for _, err := range errors {
+		cmd.Println(err)
+	}
+
 	cmd.Println("Done!")
+
+	return nil
 }
 
 func init() {
